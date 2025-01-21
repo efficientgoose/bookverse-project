3f041f6349dc280519b8ba1426e85d554785fc80
ajinkode <ajinkode@gmail.com>
2025-01-22 00:03:57 +0530

    Created login functionality

--- book-network-ui/src/app/pages/login/login.component.ts
+++ book-network-ui/src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationRequest } from 'src/app/services/models';
 import { AuthenticationService } from 'src/app/services/services';
+import { TokenService } from 'src/app/services/token/token.service';
 
 @Component({
   selector: 'app-login',
@@ -14,7 +15,8 @@ export class LoginComponent {
 
   constructor(
     private router: Router,
-    private authService: AuthenticationService
+    private authService: AuthenticationService,
+    private tokenService: TokenService
   ) {}
 
   login() {
@@ -25,7 +27,8 @@ export class LoginComponent {
       })
       .subscribe({
         next: (res) => {
-          //todo save the token
+          this.tokenService.token = res.token as string;
+
           this.router.navigate(['books']);
           console.log(res);
         },
